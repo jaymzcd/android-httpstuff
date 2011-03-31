574addb1159a641fbca18bbd06b67e7dc1529659
jaymz <jaymz@u-dox.com>
2011-03-31 10:21:20 +0100

    initial commit

---
+++ src/httpfun/jaymz/eu/HttpFun.java
@@ -0,0 +1,18 @@
+package httpfun.jaymz.eu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.apache.http.impl.DefaultHttpClient;
+
+public class HttpFun extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    
+    }
+}
